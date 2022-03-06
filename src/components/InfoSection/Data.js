5764dc966c8b40ebbdc7a8a43eb3446d6a07c3d3
Ian Camargo <ian.camargo.andrade@gmail.com>
2022-03-06 17:02:17 -0300

    teste2

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne={
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited blablabla blbabla balblaba',
+    description: ' blalbalblabll babla b lb a bl albalblbbalb abalbablalbalblabll babla b lb a bl albalblbbalb abalbablalbalblabll babla b lb a bl albalblbbalb abalba',
+    buttonLabel: 'Get started',
+    imStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
